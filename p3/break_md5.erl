42eb2a72591b518b23a354a00223416e35edfe61
evaRce <eva.arce123@hotmail.com>
2022-03-15 12:12:03 +0100

    ej1

--- p3/break_md5.erl
+++ p3/break_md5.erl
@@ -3,7 +3,7 @@
 -define(UPDATE_BAR_GAP, 1000).
 -define(BAR_SIZE, 40).
 
--export([break_md5/1,
+-export([break_md5s/1,
          pass_to_num/1,
          num_to_pass/1,
          num_to_hex_string/1,
@@ -23,8 +23,8 @@ pow_aux(Base, Pow, Exp) ->
 
 pow(Base, Exp) -> pow_aux(Base, 1, Exp).
 
-%% Number to password and back conversion
 
+%% Number to password and back conversion
 num_to_pass_aux(_N, 0, Pass) -> Pass;
 num_to_pass_aux(N, Digit, Pass) ->
     num_to_pass_aux(N div 26, Digit - 1, [$a + N rem 26 | Pass]).
@@ -34,8 +34,8 @@ num_to_pass(N) -> num_to_pass_aux(N, ?PASS_LEN, []).
 pass_to_num(Pass) ->
     lists:foldl(fun (C, Num) -> Num * 26 + C - $a end, 0, Pass).
 
-%% Hex string to Number
 
+%% Hex string to Number
 hex_char_to_int(N) ->
     if (N >= $0) and (N =< $9) -> N - $0;
        (N >= $a) and (N =< $f) -> N - $a + 10;
@@ -49,6 +49,7 @@ int_to_hex_char(N) ->
        true                   -> throw({out_of_range, N})
     end.
 
+%
 hex_string_to_num(Hex_Str) ->
     lists:foldl(fun(Hex, Num) -> Num*16 + hex_char_to_int(Hex) end, 0, Hex_Str).
 
@@ -63,22 +64,22 @@ num_to_hex_string(N) -> num_to_hex_string_aux(N, []).
 %% Progress bar runs in its own process
 
 progress_loop(N, Bound) ->
-    receive
+    receive     % it is used to allow processes to wait for messages from other processes
         stop -> ok;
         {progress_report, Checked} ->
             N2 = N + Checked,
             Full_N = N2 * ?BAR_SIZE div Bound,
-            Full = lists:duplicate(Full_N, $=),
+            Full = lists:duplicate(Full_N, $=), %Returns a list containing N(Full_N) copies of term Elem($=).
             Empty = lists:duplicate(?BAR_SIZE - Full_N, $-),
             io:format("\r[~s~s] ~.2f%", [Full, Empty, N2/Bound*100]),
             progress_loop(N2, Bound)
     end.
 
-%% break_md5/2 iterates checking the possible passwords
 
+%% break_md5/2 iterates checking the possible passwords
 break_md5(Target_Hash, N, N, _) -> {not_found, Target_Hash};  % Checked every possible password
 break_md5(Target_Hash, N, Bound, Progress_Pid) ->
-    if N rem ?UPDATE_BAR_GAP == 0 ->
+    if N rem ?UPDATE_BAR_GAP == 0 ->   %rem =  módulo
             Progress_Pid ! {progress_report, ?UPDATE_BAR_GAP};
        true ->
             ok
@@ -93,12 +94,22 @@ break_md5(Target_Hash, N, Bound, Progress_Pid) ->
             break_md5(Target_Hash, N+1, Bound, Progress_Pid)
     end.
 
-%% Break a hash
 
+%% Break a hash
 break_md5(Hash) ->
     Bound = pow(26, ?PASS_LEN),
-    Progress_Pid = spawn(?MODULE, progress_loop, [0, Bound]),
+    Progress_Pid = spawn(?MODULE, progress_loop, [0, Bound]),  %[0, Bound] son los argumentos para progess_loop
     Num_Hash = hex_string_to_num(Hash),
     Res = break_md5(Num_Hash, 0, Bound, Progress_Pid),
     Progress_Pid ! stop,
     Res.
+
+%ej1
+break_md5s(Hashes)->
+    Bound = pow(26, ?PASS_LEN),
+    Progress_Pid = spawn(?MODULE, progress_loop, [0, Bound]),  %[0, Bound] son los argumentos para progess_loop
+    %Num_Hashes = lists:map(fun hex_string_to_num/1, Hashes),
+    Res_List = lists:map(fun break_md5/1, Hashes),
+    Progress_Pid ! stop,
+    Res_List.
+
